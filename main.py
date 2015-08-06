5c7692e2ac2e76a79b66f52e56539779f06ea59c
Ed Brannin <edbrannin@gmail.com>
2015-08-06 08:35:18 -0400

    html conversion with pandoc

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from lxml import html, etree
 import requests
 
+try:
+    import pypandoc as converter
+except ImportError:
+    import pydocverter as converter
+
 page = requests.get('http://nfcmusa.org/sunday-readings-questions')
 tree = html.fromstring(page.text)
 
 #This will create a list of prices
 reflection_html = tree.xpath('//div[@class="moduleBody"]')[0]
 
-print etree.tostring(reflection_html, pretty_print=True)
+html = etree.tostring(reflection_html, pretty_print=True)
+
+markdown = converter.convert(html, 'markdown', format='html')
+
+print markdown
+
